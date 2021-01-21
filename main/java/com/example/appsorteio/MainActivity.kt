31cdcdab4ee2a9e82a646d3fc09ad033ad8bdb42
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2021-01-21 20:53:41 -0300

    Projeto App Sorteio

---
+++ main/java/com/example/appsorteio/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appsorteio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+        var texto = findViewById(R.id.textoSorteado) as TextView
+        var numeroSorteado = Random().nextInt(11)
+        texto.setText("Número sorteado é: $numeroSorteado")
+    }
+}
